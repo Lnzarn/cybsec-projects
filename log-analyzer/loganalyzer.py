95ab19429fb363b14632773662d12d743ebf0e25
Lnzarn <enriquelanzaron@gmail.com>
2025-12-04 00:48:33 +0800

    Log analyzer: Added a function to check for admin rights, and try catching errors.

--- log-analyzer/loganalyzer.py
+++ log-analyzer/loganalyzer.py
@@ -1,7 +1,17 @@
 import win32evtlog  # requires to have pywin32 installed.
 import win32api
 import win32con
+# needed for the security permission (but still requires to run as admin).
 import win32security
+import ctypes
+import os  # modules for checking admin rights
+
+
+def checkforAdminRights():
+    try:
+        return os.getuid() == 0
+    except AttributeError:
+        return ctypes.windll.shell32.IsUserAnAdmin()
 
 
 def securityPermission():
@@ -22,24 +32,32 @@ def getTotalsOfSources(server, logTypes):
 
 
 def main(server, logTypes):
-    print("Sources:\n")
-    securityPermission()
-    getTotalsOfSources(server, logTypes)
+    try:
+        print("Sources:\n")
+        if (checkforAdminRights() != True):
+            raise SystemExit("Exiting due to not running as administrator.")
+        securityPermission()
+        getTotalsOfSources(server, logTypes)
+        logno = None
+        while logno not in [0, 1, 2]:
+            logno = int(input(
+                "Which source would you like to use? [1 - Application, 2 - System, 3 - Security]\n>"))-1
 
-    logno = int(input(
-        "Which source would you like to use? [1 - Application, 2 - System, 3 - Security]\n>"))-1
+        # this reads the logs sequentially from newest to oldest.
+        evtHandle = win32evtlog.OpenEventLog(server, logTypes[logno])
+        flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
 
-    # this reads the logs sequentially from newest to oldest.
-    evtHandle = win32evtlog.OpenEventLog(server, logTypes[logno])
-    flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
+        logs = win32evtlog.ReadEventLog(evtHandle, flags, 0)
 
-    logs = win32evtlog.ReadEventLog(evtHandle, flags, 0)
-
-    for event in logs:
-        print(f"evtID: {event.EventID}\ntime: {event.TimeGenerated}\nsource: {event.SourceName}\n"
-              f"evtCat: {event.EventCategory}\ncomputer: {event.ComputerName}\n"
-              f"recNo: {event.RecordNumber}\ntext: {event.StringInserts}")
-        print("\n"+"_"*60)
+        for event in logs:
+            print(f"evtID: {event.EventID}\ntime: {event.TimeGenerated}\nsource: {event.SourceName}\n"
+                  f"evtCat: {event.EventCategory}\ncomputer: {event.ComputerName}\n"
+                  f"recNo: {event.RecordNumber}\ntext: {event.StringInserts}")
+            print("\n"+"_"*60)
+    except SystemExit as e:
+        print(f"Error: {e}")
+    except ValueError as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
